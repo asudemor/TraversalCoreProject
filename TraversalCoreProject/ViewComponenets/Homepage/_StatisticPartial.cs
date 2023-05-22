ef96f91e861df1e1ac3e947006cfb11671c5a47c
asudemor <asude-bjk@hotmail.com>
2023-05-22 23:49:42 +0300

    https://www.youtube.com/watch?v=2uDbe_9HetY&list=PLKnjBHu2xXNMK5MBogdXmsXVi3K_eEZT5&index=21 videoda kalındı.

---
+++ TraversalCoreProject/ViewComponenets/Homepage/_StatisticPartial.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TraversalCoreProject.ViewComponenets.Homepage
+{
+    public class _StatisticPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            using var c = new Context();
+            ViewBag.v1 = c.Destinations.Count();
+            ViewBag.v2 = c.Guides.Count();
+            ViewBag.v3 = "284";
+            return View();
+        } 
+    }
+}
---
+++ TraversalCoreProject/ViewComponenets/Homepage/_SubAboutPartial.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponenets.Homepage
+{
+    public class _SubAboutPartial : ViewComponent
+    {
+        SubAboutManager subaboutManager = new SubAboutManager(new EfSubAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = subaboutManager.TGetList();
+            return View(values);
+        }
+    }
+}
