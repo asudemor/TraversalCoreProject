922215af876dbb9e3c7b22ec4a47c05f5b2c7d60
asudemor <asude-bjk@hotmail.com>
2023-06-24 15:09:05 +0300

    https://www.youtube.com/watch?v=epBrrORnSQM&list=PLKnjBHu2xXNMK5MBogdXmsXVi3K_eEZT5&index=26

--- TraversalCoreProject/ViewComponenets/Comment/_AddComment.cs
+++ TraversalCoreProject/ViewComponenets/Comment/_AddComment.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.RulesetToEditorconfig;
+using System;
 
 namespace TraversalCoreProject.ViewComponenets.Comment
 {
     public class _AddComment : ViewComponent
     {
+        [HttpGet]
         public IViewComponentResult Invoke()
         {
             return View();
